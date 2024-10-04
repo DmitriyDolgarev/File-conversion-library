8aaa8abd4f83788ed236178d734c359b78ba6f09
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-10-04 21:04:59 +0800

    Added JPG file to PNG file and PNG file to JPG file

--- FileConverterLib/FileConverter.cs
+++ FileConverterLib/FileConverter.cs
@@ -1,10 +1,50 @@
-﻿namespace FileConverterLib
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FileConverterLib
 {
     public class FileConverter
     {
-        public void test()
+        #region JPG to PNG
+        public static void JpgFileToPngFile(string jpgFileName, string pngFileName)
         {
-            Console.WriteLine("123");
+            if (Path.GetExtension(jpgFileName) == "")
+                jpgFileName += ".jpg";
+            if (Path.GetExtension(pngFileName) == "")
+                pngFileName += ".png";
+
+            using (var img = Image.FromFile(jpgFileName))
+            {
+                img.Save(pngFileName, ImageFormat.Png);
+            }
         }
+
+        public static void JpgFileToPngFile(string jpgFileName)
+        {
+            string pngFileName = $@"{Path.GetDirectoryName(jpgFileName)}\{Path.GetFileNameWithoutExtension(jpgFileName)}.png";
+            JpgFileToPngFile(jpgFileName, pngFileName);
+        }
+        #endregion
+
+        #region PNG TO JPG
+        public static void PngFileToJpgFile(string pngFileName, string jpgFileName)
+        {
+            if (Path.GetExtension(pngFileName) == "")
+                pngFileName += ".png";
+            if (Path.GetExtension(jpgFileName) == "")
+                jpgFileName += ".jpg";
+
+            using (var img = Image.FromFile(pngFileName))
+            {
+                img.Save(jpgFileName, ImageFormat.Jpeg);
+            }
+        }
+
+        public static void PngFileToJpgFile(string pngFileName)
+        {
+            string jpgFileName = $@"{Path.GetDirectoryName(pngFileName)}\{Path.GetFileNameWithoutExtension(pngFileName)}.jpg";
+            PngFileToJpgFile(pngFileName, jpgFileName);
+        }
+        #endregion
     }
 }

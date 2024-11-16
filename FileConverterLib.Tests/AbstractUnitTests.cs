af2030d992214be61b59c6c182660e10e8d5ffb7
Ana <dns80791@gmail.com>
2024-11-16 21:51:23 +0800

    splites tests as Library Modules

---
+++ FileConverterLib.Tests/AbstractUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConverterLib.Tests
+{
+    [TestClass]
+    public abstract class AbstractUnitTests
+    {
+        protected static string pathTestfiles = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "FileConverterLib.Tests", "testfiles"));
+        protected static string pathResult = Path.GetFullPath(Path.Combine("..", "..", "..", "..", "FileConverterLib.Tests", "resultfiles"));
+
+        [ClassInitialize(InheritanceBehavior.BeforeEachDerivedClass)]
+        public static void BeforeTests(TestContext _context)
+        {         
+            //создаём папку в которой будут хранится все файлы для тестов
+            if (!Directory.Exists(pathResult))
+                Directory.CreateDirectory(pathResult);
+
+            // удаляем все тестовые файлы
+            foreach (string f in Directory.GetFiles(pathResult))
+                File.Delete(f);
+
+            //удаляем все тестовые папки
+            foreach (string d in Directory.GetDirectories(pathResult))
+                Directory.Delete(d, true);
+        }
+    }
+}
---
+++ FileConverterLib.Tests/UnitTestsLibreOffice.cs
@@ -0,0 +1,80 @@
+using FileConverterLib.LibreOffice;
+
+namespace FileConverterLib.Tests
+{
+    [TestClass]
+    public class UnitTestsLibreOffice : AbstractUnitTests
+    {
+        #region Test_PdfToWord_LibreOffice
+        [TestMethod]
+        public void Test_PdfToWord_LibreOffice_2params()
+        {
+            string filename = "test_pdf_1";
+            LibreOfficeConverter.PdfFileToDocxFile(Path.Combine(pathTestfiles, filename), Path.Combine(pathResult, filename));
+
+            Assert.IsTrue(File.Exists(Path.Combine(pathResult, $"{filename}.docx")));
+
+        }
+
+        [TestMethod]
+        public void Test_PdfToWord_LibreOffice_1param()
+        {
+            string filename = "test_pdf_1";
+            File.Copy(Path.Combine(pathTestfiles, $"{filename}.pdf"), Path.Combine(pathResult, $"{filename}.pdf"));
+
+            LibreOfficeConverter.PdfFileToDocxFile(Path.Combine(pathResult, filename));
+
+            Assert.IsTrue(File.Exists(Path.Combine(pathResult, $"{filename}.docx")));
+
+        }
+        #endregion
+
+        #region Test_WordToPdf_LibreOffice
+        [TestMethod]
+        public void Test_WordToPdf_LibreOffice_2params()
+        {
+            string filename = "test_word";
+            LibreOfficeConverter.DocxFileToPdfFile(Path.Combine(pathTestfiles, filename), Path.Combine(pathResult, filename));
+
+            Assert.IsTrue(File.Exists(Path.Combine(pathResult, $"{filename}.pdf")));
+
+        }
+
+        [TestMethod]
+        public void Test_WordToPdf_LibreOffice_1param()
+        {
+            string filename = "test_word";
+            File.Copy(Path.Combine(pathTestfiles, $"{filename}.docx"), Path.Combine(pathResult, $"{filename}.docx"));
+
+            LibreOfficeConverter.DocxFileToPdfFile(Path.Combine(pathResult, filename));
+
+            Assert.IsTrue(File.Exists(Path.Combine(pathResult, $"{filename}.pdf")));
+
+        }
+        #endregion
+
+        #region Test_PptxToPdf_LibreOffice
+        [TestMethod]
+        public void Test_PptxToPdf_LibreOffice_2params()
+        {
+            string filename = "test_powerpoint";
+            LibreOfficeConverter.PptxFileToPdfFile(Path.Combine(pathTestfiles, filename), Path.Combine(pathResult, filename));
+
+            Assert.IsTrue(File.Exists(Path.Combine(pathResult, $"{filename}.pdf")));
+
+        }
+
+        [TestMethod]
+        public void Test_PptxToPdf_LibreOffice_1param()
+        {
+            string filename = "test_powerpoint";
+            File.Copy(Path.Combine(pathTestfiles, $"{filename}.pptx"), Path.Combine(pathResult, $"{filename}.pptx"));
+
+            LibreOfficeConverter.PptxFileToPdfFile(Path.Combine(pathResult, filename));
+
+            Assert.IsTrue(File.Exists(Path.Combine(pathResult, $"{filename}.pdf")));
+
+        }
+        #endregion
+    }
+}
